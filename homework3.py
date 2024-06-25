349e126eb412c020dd89fa1398144bec6ddfa59f
SokolovD-V <danila-20-00@mail.ru>
2024-06-25 19:45:51 +0600

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+my_string=input("Что-то надо написать: ")
+print("Длина строки:", len(my_string))
+print("Верхний регистр: ", my_string.upper())
+print("Нижний регистр:", my_string.lower())
+print("Без пробелов:", my_string.replace(" ",""))
+print("Первый символ:", my_string[0])
+print("Последний символ:", my_string[-1])
